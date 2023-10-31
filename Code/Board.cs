d17194cceaca25becadd02225caaaa6649cbad87
Ivan <aokiroyal16@gmail.com>
2023-10-31 15:00:59 +0200

    Added base structure

---
+++ Code/Board.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_WPF.Code
+{
+    internal class Board
+    {
+        const int SIZE_X = 10;
+        const int SIZE_Y = 20;
+        const double InitialDelay = 1000;
+
+        private readonly bool[][] board; // true - occupied, false - empty
+        private Tetromino currentPiece;
+        private Tetromino nextPiece;
+        private int score;
+        private int level;
+        private int lines;
+        private double delay;
+        private readonly Random generator;
+
+        public Board(int Seed)
+        {
+            score = 0;
+            level = 0;
+            lines = 0;
+            delay = InitialDelay;
+
+            board = new bool[SIZE_Y][];
+            for (int row = 0; row < SIZE_Y; row++) board[row] = new bool[SIZE_X];
+
+            generator = new Random(Seed);
+            currentPiece = GenerateNewTetromino();
+            nextPiece = GenerateNewTetromino();
+        }
+
+        public Board() : this((int)DateTimeOffset.UtcNow.ToUnixTimeSeconds())
+        { }
+
+        private Tetromino GenerateNewTetromino()
+        {
+            Array values = Enum.GetValues(typeof(Tetromino.Types));
+            Tetromino.Types type = (Tetromino.Types)values.GetValue(generator.Next(values.Length));
+
+            Tetromino t = new Tetromino(type);
+            return t;
+        }
+    }
+}
---
+++ Code/Tetrominoes.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_WPF.Code
+{
+    internal class Coords
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    internal class Tetromino
+    {
+        const int InitialX = 10;
+        const int SIZE = 4;
+        public enum Types
+        {
+            I, J, L, O, S, T, Z
+        }
+
+        private readonly Types type;
+        private readonly Coords[] coords;
+
+        public Tetromino(Types type)
+        {
+            coords = new Coords[SIZE];
+            this.type = type;
+
+            switch (type)
+            {
+                case Types.I:
+                    for (int i = 0; i < SIZE; i++)
+                    {
+                        coords[i] = new Coords(InitialX + i, 0);
+                    }
+                    break;
+                case Types.J:
+                    for (int i = 0; i < SIZE - 1; i++)
+                    {
+                        coords[i] = new Coords(InitialX + i, 1);
+                    }
+                    coords[SIZE - 1] = new Coords(InitialX, 0);
+                    break;
+                case Types.L:
+                    for (int i = 0; i < SIZE - 1; i++)
+                    {
+                        coords[i] = new Coords(InitialX + i, 1);
+                    }
+                    coords[SIZE - 1] = new Coords(InitialX + SIZE - 2, 0);
+                    break;
+                case Types.O:
+                    for (int i = 0; i < SIZE / 2; i++)
+                    {
+                        coords[i] = new Coords(InitialX + i, 0);
+                        coords[i + SIZE / 2] = new Coords(InitialX + i, 1);
+                    }
+                    break;
+                case Types.S:
+                    for (int i = 0; i < SIZE / 2; i++)
+                    {
+                        coords[i] = new Coords(InitialX + i, 1);
+                        coords[i + SIZE / 2] = new Coords(InitialX + i + SIZE / 2 - 1, 0);
+                    }
+                    break;
+                case Types.T:
+                    for (int i = 0; i < SIZE - 1; i++)
+                    {
+                        coords[i] = new Coords(InitialX + i, 1);
+                    }
+                    coords[SIZE - 1] = new Coords(InitialX + SIZE / 2 - 1, 0);
+                    break;
+                case Types.Z:
+                    for (int i = 0; i < SIZE / 2; i++)
+                    {
+                        coords[i] = new Coords(InitialX + i + SIZE / 2 - 1, 1);
+                        coords[i + SIZE / 2] = new Coords(InitialX + i, 0);
+                    }
+                    break;
+            }
+
+        }
+
+    }
+}
